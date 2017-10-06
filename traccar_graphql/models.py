cd20661127fb21a08345a52c846fcd52ad3f74f7
Sun Howwrongbum <sun@adrenosapient.com>
2017-10-07 02:08:47 +0530

    Add groups query
    - factor out groups resolver

--- traccar_graphql/models.py
+++ traccar_graphql/models.py
@@ -1,5 +1,8 @@
+import requests
 from graphene import Interface, ObjectType, String, Boolean, Int, Float, Field
 from graphene.types.datetime import DateTime
+
+from traccar_graphql.resolvers import resolve_groups
 
 class SettingsType(Interface):
     id = Int()
@@ -40,3 +43,12 @@ class UserType(ObjectType):
     device_limit = Int()
     expiration_time = DateTime()
     token = String()
+
+class GroupType(ObjectType):
+    id = Int()
+    name = String()
+    parent_group = Field(lambda: GroupType)
+
+    def resolve_parent_group(self, args, context, info):
+        groups = resolve_groups(self, args, context, info)
+        return next((x for x in groups if x.id == self.group_id), None)
--- traccar_graphql/schema.py
+++ traccar_graphql/schema.py
@@ -1,9 +1,10 @@
 import os, graphene, requests
-from flask_jwt_extended import get_jwt_identity, get_jwt_claims
+from flask_jwt_extended import get_jwt_claims
 from graphql import GraphQLError
 
-from traccar_graphql.models import ServerType, UserType
+from traccar_graphql.models import ServerType, UserType, GroupType
 from traccar_graphql.mutations import LoginType, RegisterType
+from traccar_graphql.resolvers import resolve_groups
 from traccar_graphql.utils import request2object
 
 TRACCAR_BACKEND = os.environ.get('TRACCAR_BACKEND')
@@ -24,6 +25,9 @@ class Query(graphene.ObjectType):
         if r.status_code == 404:
             raise GraphQLError('Authentication required')
         return request2object(r, 'UserType')
+
+    all_groups = graphene.List(lambda: GroupType)
+    resolve_all_groups = resolve_groups
 
 class Mutation(graphene.ObjectType):
     login = LoginType.Field()
