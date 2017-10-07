b38c29f106ecd4c6f94f2ba0be60aae3d69c59dc
Sun Howwrongbum <sun@adrenosapient.com>
2017-10-07 23:22:34 +0530

    Update tests to use pytest
    - write some tests for queries

---
+++ traccar_graphql/tests/test_queries.py
@@ -0,0 +1,55 @@
+from graphene.test import Client
+
+from traccar_graphql.schema import schema
+
+def test_query_server(snapshot):
+    """Testing query for server"""
+    client = Client(schema)
+    snapshot.assert_match(client.execute('''{ server {
+        id
+        map
+        latitude
+        longitude
+        zoom
+        readonly
+        deviceReadonly
+        distanceUnit
+        speedUnit
+        timezone
+        twelveHourFormat
+        coordinateFormat
+        version
+        mapUrl
+        registration
+        forceSettings
+        bingKey
+        limitCommands
+        } }'''))
+
+def test_query_me(snapshot):
+    """Testing query for me"""
+    client = Client(schema)
+    snapshot.assert_match(client.execute('''{ me {
+        id
+        map
+        latitude
+        longitude
+        zoom
+        readonly
+        deviceReadonly
+        distanceUnit
+        speedUnit
+        timezone
+        twelveHourFormat
+        coordinateFormat
+        email
+        name
+        phone
+        password
+        admin
+        disabled
+        userLimit
+        deviceLimit
+        expirationTime
+        token
+        } }'''))
--- traccar_graphql/tests/test_utils.py
+++ traccar_graphql/tests/test_utils.py
@@ -2,38 +2,24 @@ import unittest
 
 from traccar_graphql.utils import camelify_keys
 
-class TestUtilsCamelifyKeys(unittest.TestCase):
+class TestUtilsCamelifyKeys:
     def test_flat_dict(self):
-        self.assertEqual(
-            camelify_keys({ 'first_name': "Tony", 'last_name': "Stark" }),
+        assert camelify_keys({ 'first_name': "Tony", 'last_name': "Stark" }) == \
             { 'firstName': "Tony", 'lastName': "Stark" }
-            )
     def test_nested_dict(self):
-        self.assertEqual(
-            camelify_keys({ 'org_name': "S.H.I.E.L.D.", 'contact': { 'first_name': "Maria" } }),
+        assert camelify_keys({ 'org_name': "S.H.I.E.L.D.", 'contact': { 'first_name': "Maria" } }) == \
             { 'orgName': "S.H.I.E.L.D.", 'contact': { 'firstName': "Maria" } }
-            )
     def test_list_of_dicts(self):
-        self.assertEqual(
-            camelify_keys({ 'assets': [{ 'unique_id': 'MK_VIII', 'status': 'active' }, { 'unique_id': 'MK_XIIc', 'status': 'unknown' }] }),
+        assert camelify_keys({ 'assets': [{ 'unique_id': 'MK_VIII', 'status': 'active' }, { 'unique_id': 'MK_XIIc', 'status': 'unknown' }] }) == \
             { 'assets': [{ 'uniqueId': 'MK_VIII', 'status': 'active' }, { 'uniqueId': 'MK_XIIc', 'status': 'unknown' }] }
-            )
     def test_complex(self):
-        self.assertEqual(
-            camelify_keys({
-                'first_name': "Tony",
-                'affliated_to': { 'org_name': "Stark Industries", 'affliations': [{ 'org_name': "Avengers" }, { 'org_name': "S.H.I.E.L.D.", 'contact': { 'first_name': "Maria" } }] },
-                'likes': ["Shawarma", { 'id': "MK_I", "top_speed": 300 }],
-                'keys': [[{ 'primary_key': 23, 'access': "Parker Residence"}, { 'secondary_key': 1 }], [{ 'primary_key': 12, 'access': "Avenger Tower"}]]
-                }),
+        assert camelify_keys({
+            'first_name': "Tony",
+            'affliated_to': { 'org_name': "Stark Industries", 'affliations': [{ 'org_name': "Avengers" }, { 'org_name': "S.H.I.E.L.D.", 'contact': { 'first_name': "Maria" } }] },
+            'likes': ["Shawarma", { 'id': "MK_I", "top_speed": 300 }],
+            'keys': [[{ 'primary_key': 23, 'access': "Parker Residence"}, { 'secondary_key': 1 }], [{ 'primary_key': 12, 'access': "Avenger Tower"}]] }) == \
             {
-                'firstName': "Tony",
-                'affliatedTo': { 'orgName': "Stark Industries", 'affliations': [{ 'orgName': "Avengers" }, { 'orgName': "S.H.I.E.L.D.", 'contact': { 'firstName': "Maria" } }] },
-                'likes': ["Shawarma", { 'id': "MK_I", "topSpeed": 300 }],
-                'keys': [[{ 'primaryKey': 23, 'access': "Parker Residence"}, { 'secondaryKey': 1 }], [{ 'primaryKey': 12, 'access': "Avenger Tower"}]]
-                }
-            )
-
-
-if __name__ == '__main__':
-    unittest.main()
+            'firstName': "Tony",
+            'affliatedTo': { 'orgName': "Stark Industries", 'affliations': [{ 'orgName': "Avengers" }, { 'orgName': "S.H.I.E.L.D.", 'contact': { 'firstName': "Maria" } }] },
+            'likes': ["Shawarma", { 'id': "MK_I", "topSpeed": 300 }],
+            'keys': [[{ 'primaryKey': 23, 'access': "Parker Residence"}, { 'secondaryKey': 1 }], [{ 'primaryKey': 12, 'access': "Avenger Tower"}]] }
