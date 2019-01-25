97245e28a18bab220a5e757f7605ed6f6fa3c60b
Sun Howwrongbum <sun@libermatic.com>
2019-01-26 01:45:38 +0530

    Update setup.py
    - add dep toolz

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
+import re
+import ast
 from setuptools import find_packages, setup
 
-__version__ = "0.0.1"
+
+with open("traccar_graphql/__init__.py", "rb") as f:
+    version = ast.literal_eval(
+        re.compile(r"VERSION\s+=\s+(.*)").search(f.read().decode("utf-8")).group(1)
+    )
 
 setup(
     name="traccar_graphql",
-    version=__version__,
+    version=version,
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
@@ -14,5 +20,6 @@ setup(
         "flask_graphql",
         "requests",
         "flask-jwt-extended",
+        "toolz",
     ],
 )
