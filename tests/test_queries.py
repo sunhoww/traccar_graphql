7d603470ddf95cab075a9e94cf8de11e0b099eb9
Sun Howwrongbum <sun@libermatic.com>
2019-01-30 20:35:08 +0530

    Move test files outside module

--- tests/test_queries.py
+++ tests/test_queries.py
@@ -6,7 +6,9 @@ from traccar_graphql.schema import schema
 def test_query_server(snapshot):
     """Testing query for server"""
     client = Client(schema)
-    snapshot.assert_match(client.execute('''{ server {
+    snapshot.assert_match(
+        client.execute(
+            """{ server {
         id
         map
         latitude
@@ -25,13 +27,17 @@ def test_query_server(snapshot):
         forceSettings
         bingKey
         limitCommands
-        } }'''))
+        } }"""
+        )
+    )
 
 
 def test_query_me(snapshot):
     """Testing query for me"""
     client = Client(schema)
-    snapshot.assert_match(client.execute('''{ me {
+    snapshot.assert_match(
+        client.execute(
+            """{ me {
         id
         map
         latitude
@@ -54,4 +60,6 @@ def test_query_me(snapshot):
         deviceLimit
         expirationTime
         token
-        } }'''))
+        } }"""
+        )
+    )
